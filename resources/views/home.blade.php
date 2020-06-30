52814089419b84c0af82d92362056387d74d3dc0
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-06-30 17:00:14 +0200

    jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.page_layout')
 @section('page_title', 'Home')
 @section('page')
-    
+    @include('partials.jumbo')
 @endsection
---
+++ resources/views/partials/jumbo.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="jumbo">
+        <div class="jumbo_left">
+            <h1>DIVENTA <strong>SVILUPPATORE WEB</strong></h1>
+            <h3>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</h3>
+            <ul>
+                <li><strong>6 mesi</strong> di corso intensivo online in diretta</li>
+                <li><strong>Nessuna competenza</strong> di programmazione richiesta</li>
+                <li>Siamo certi del tuo successo, altrimenti<strong> ti rimborsiamo</strong></li>
+            </ul>
+        </div>
+        <div class="jumbo_right">
+            <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="">
+        </div>
+    </div>
+</div>
